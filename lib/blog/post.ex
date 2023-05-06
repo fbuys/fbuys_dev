d633a5e4e31b4c332a805e77ba469c34653df87d
Francois Buys <buys.fran@gmail.com>
2023-05-07 00:02:49 +0200

    Parse excerpt from post

--- lib/blog/post.ex
+++ lib/blog/post.ex
@@ -1,15 +1,28 @@
 defmodule FbuysDev.Blog.Post do
-  @enforce_keys [:id, :author, :title, :body, :description, :tags, :date, :path]
-  defstruct [:id, :author, :title, :body, :description, :tags, :date, :path]
+  @enforce_keys [:id, :author, :title, :body, :tags, :date, :path]
+  defstruct [:id, :author, :title, :body, :excerpt, :tags, :date, :path]
 
   def build(filename, attrs, body) do
-    IO.inspect(filename, label: 'build-filename')
-    IO.inspect(attrs, label: 'build-attrs')
     path = Path.rootname(filename)
     [year, month_day_id] = path |> Path.split() |> Enum.take(-2)
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
     path = path <> ".html"
-    struct!(__MODULE__, [id: id, date: date, body: body, path: path] ++ Map.to_list(attrs))
+    excerpt = excerpt(attrs, body)
+
+    IO.inspect(excerpt)
+
+    struct!(
+      __MODULE__,
+      [id: id, date: date, body: body, path: path, excerpt: excerpt] ++ Map.to_list(attrs)
+    )
+  end
+
+  defp excerpt(%{excerpt: excerpt}, _body), do: excerpt
+
+  defp excerpt(_attrs, body) do
+    body
+    |> String.split("<!-- more -->")
+    |> (fn [excerpt | _] -> excerpt end).()
   end
 end
