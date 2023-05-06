e89a6908b140e7431c29e552e2fcb7f095d4ec29
Francois Buys <buys.fran@gmail.com>
2023-05-06 16:14:09 +0200

    🎉 Start

---
+++ lib/fbuys_dev.ex
@@ -0,0 +1,18 @@
+defmodule FbuysDev do
+  @moduledoc """
+  Documentation for `FbuysDev`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FbuysDev.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
