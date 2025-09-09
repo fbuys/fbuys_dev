147e8b1ee51a398d762dd402f3da566ea41e73cc
Francois Buys <buys.fran@gmail.com>
2025-09-09 11:41:00 +0200

    (feat) Scaffold blog posts
    - Add blog post index
    - Add ability to view a blog post
    - Migrate one blog post from previous blog site
    
    *Add blog/posts index*
    Introduce a basic blog system using the Decant gem for static content
    management. Add Kramdown and kramdown-parser-gfm for Markdown rendering.
    Implement a Blog model, controller, and view to render Markdown files
    from the `blog` directory. Update routes to serve the blog index at
    /blog. Add a sample index.md file for demonstration.
    
    *Add first blog post*
    Refactored blog model to use Decant for posts with frontmatter. Added
    Blog::Post and Blog::Content models for Markdown and ERB rendering.
    Updated routes and controllers to support grouped posts by year and
    individual post pages. Removed legacy index and improved views for
    listing and showing blog posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :blog do
+    get "/", controller: "posts", action: "index", as: :posts
+    get "/posts/:year/:slug", controller: "posts", action: "show", as: :post
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
